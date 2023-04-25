2386bd5dfd8e6e5bd6677410ee6b333a72bfcea8
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2023-04-25 15:28:35 -0400

    Feat: Noise Effect in Background

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Container from "~/components/Container.vue";
+import NoiseTexture from "~/components/NoiseTexture.vue";
 </script>
 <template>
   <main class="min-h-screen bg-black p-4 md:p-2">
-    <Container>
+    <Container class="z-10">
       <NuxtPage />
     </Container>
+    <NoiseTexture />
   </main>
 </template>
